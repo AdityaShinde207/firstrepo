74261c957672addd7991b87ad6ef8ec9976ddcbf
ADITYA SHINDE <adityashinde207@gmail.com>
2021-06-16 15:02:42 +0530

    Added Funcction Headers

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#Add Implementation
 def add(x,y):
     pass
+
+#Sub Implementation
 def subtract(x,y):
     pass
+
+#Mul Implementation
 def multiply(x,y):
     pass
+    
+#divide Implementation
 def divide(x,y):
     pass
